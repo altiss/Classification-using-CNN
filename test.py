f435b2cace233985d67a2f636b97d08dacaa3621
altiss <sistlaanirudh1982@gmail.com>
2022-11-07 13:54:56 -0600

    9th commit

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 def method():
-    a,b=1,2
-    c,d=3,4
-    e=a+b
-    return a+b+c-d-e
+    a, b = 1, 2
+    c, d = 3, 4
+    e = a + b
+    f = a + b + e - c
+    g = a + c + e + f
+    return a + b + c - d - e - f + g
